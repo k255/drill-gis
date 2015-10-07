1962f5d1a03e58c8127b051c5494df54c8634726
k255 <k255@gmx.com>
2015-10-07 12:24:38 +0200

    basic geometry functions implementation

--- src/main/java/org/apache/drill/contrib/function/drillgis/STUnion.java
+++ src/main/java/org/apache/drill/contrib/function/drillgis/STUnion.java
@@ -6,25 +6,20 @@ import org.apache.drill.exec.expr.DrillSimpleFunc;
 import org.apache.drill.exec.expr.annotations.FunctionTemplate;
 import org.apache.drill.exec.expr.annotations.Output;
 import org.apache.drill.exec.expr.annotations.Param;
-import org.apache.drill.exec.expr.holders.BitHolder;
-import org.apache.drill.exec.expr.holders.Float8Holder;
 import org.apache.drill.exec.expr.holders.VarBinaryHolder;
 
 import io.netty.buffer.DrillBuf;
 
-@FunctionTemplate(name = "st_dwithin", scope = FunctionTemplate.FunctionScope.SIMPLE, nulls = FunctionTemplate.NullHandling.NULL_IF_NULL)
-public class STDWithin implements DrillSimpleFunc {
+@FunctionTemplate(name = "st_union", scope = FunctionTemplate.FunctionScope.SIMPLE, nulls = FunctionTemplate.NullHandling.NULL_IF_NULL)
+public class STUnion implements DrillSimpleFunc {
 	@Param
 	VarBinaryHolder geom1Param;
-
+	
 	@Param
 	VarBinaryHolder geom2Param;
 
-	@Param(constant = true)
-	Float8Holder distanceParam;
-
 	@Output
-	BitHolder out;
+	VarBinaryHolder out;
 
 	@Inject
 	DrillBuf buffer;
@@ -33,16 +28,17 @@ public class STDWithin implements DrillSimpleFunc {
 	}
 
 	public void eval() {
-		double distance = distanceParam.value;
-
 		com.esri.core.geometry.ogc.OGCGeometry geom1;
 		com.esri.core.geometry.ogc.OGCGeometry geom2;
-		
 		geom1 = com.esri.core.geometry.ogc.OGCGeometry.fromBinary(geom1Param.buffer.nioBuffer(geom1Param.start, geom1Param.end));
-		geom2 = com.esri.core.geometry.ogc.OGCGeometry.fromBinary(geom2Param.buffer.nioBuffer(geom2Param.start, geom2Param.end));
+		geom2 = com.esri.core.geometry.ogc.OGCGeometry.fromBinary(geom1Param.buffer.nioBuffer(geom2Param.start, geom2Param.end));
 
-		int isWithin = geom1.distance(geom2) <= distance ? 1 : 0;
+		com.esri.core.geometry.ogc.OGCGeometry unionGeom = geom1.union(geom2);
 
-		out.value = isWithin;
+		java.nio.ByteBuffer bufferedGeomBytes = unionGeom.asBinary();
+		out.buffer =  buffer;
+		out.start = 0;
+		out.end = bufferedGeomBytes.remaining();
+		buffer.setBytes(0, bufferedGeomBytes);
 	}
 }
